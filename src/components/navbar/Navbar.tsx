3924d754dc59a0115776144991d2ffffb90826f8
Adrian Podlawski <apodlawski@gmail.com>
2021-06-09 00:41:22 +0200

    add flex grid  and icons

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,8 +6,12 @@ function Navbar(){
     return (
         <>
         <nav className="navbarStyle" >
-            <ul>
-                <NavbarItem link ="https://www.linkedin.com/in/adrian-podlawski-261192129/" iconClassName ="fab fa-linkedin navbarIcon"/>
+            <ul className="navbar-list">                
+                <NavbarItem link ="#personal" iconClassName ="fas fa-address-card fa-3x"/>
+                <NavbarItem link ="#history" iconClassName ="fas fa-history fa-3x"/>
+                <NavbarItem link ="#education" iconClassName ="fas fa-landmark fa-3x"/>
+                <NavbarItem link ="#contact" iconClassName ="far fa-address-book fa-3x"/>               
+                <NavbarItem link ="https://www.linkedin.com/in/adrian-podlawski-261192129/" iconClassName ="fab fa-linkedin fa-3x"/>
             </ul>
         </nav>
         </>
